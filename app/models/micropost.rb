243ca4d4cc40ee234d0e84a27d9f2c5737b5f0c4
Event <event@Event6.(none)>
2012-12-08 17:26:38 +0530

    huuuuuuuuuuuuuu done

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  validates :content, presence: true,length: {minimum: 1,maximum: 40}
+  validate :user_id,presence: true
 end
