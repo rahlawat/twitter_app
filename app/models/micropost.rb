e039b2f3a7f0a9f89576669fef1796a7142c1560
rahlawat <renu.ahlawat21@gmail.com>
2012-12-08 18:14:45 +0530

    added micropost-user relationship

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
-  validates :content, presence: true,length: {minimum: 1,maximum: 40}
-  validate :user_id,presence: true
+   validate :user_id,presence: true
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 140 }
 end
